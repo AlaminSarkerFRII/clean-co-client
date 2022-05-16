07c99400a321f5eda919fd7a3b5c95e82f2ae8f2
AlaminSarkerFRII <alamin.education1997@gmail.com>
2022-05-16 19:47:12 +0600

    add animation aos

--- src/pages/Home/Landing.js
+++ src/pages/Home/Landing.js
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import BucketGirl from "../../assets/image/bucketgirl.png";
 
+import AOS from "aos";
+import "aos/dist/aos.css";
+
 const Landing = () => {
+  // use AOS
+
+  useEffect(() => {
+    AOS.init();
+  }, []);
+
   return (
-    <>
-      <div className="hero h-screen lg:h-[60vh]  bg-accent mt-16">
+    <div className="relative">
+      <div className="hero h-screen lg:h-[60vh] z-10 relative  bg-accent mt-16">
         <div className="hero-content flex-col lg:flex-row">
-          <div>
+          <div data-aos="fade-right" data-aos-easing="ease-in-sine">
             <p>Best Quality</p>
             <h1 className="text-5xl font-bold">
               Professional Cleaning Service
@@ -26,27 +35,53 @@ const Landing = () => {
 
       {/* input field */}
 
-      <div className="p-10 rounded-2xl shadow-lg mt-[-30px] w-5/6 mx-auto z-50 bg-neutral">
-        <h2 className="text-primary text-2xl p-2.5">Get Features</h2>
-        <div>
+      <div className="p-10 rounded-2xl relative shadow-lg mt-[-30px] w-5/6 mx-auto z-20 bg-base-200">
+        <h2 className="text-2xl mb-5">Get Features</h2>
+        <div className="grid grid-cols-1 lg:grid-cols-4 md:grid-cols-2 gap-4 mt-5 ">
           <input
             type="text"
             placeholder="Type here"
-            class="input input-bordered w-full max-w-xs"
+            class="input input-bordered w-full"
           />
           <input
             type="text"
             placeholder="Type here"
-            class="input input-bordered w-full max-w-xs"
+            class="input input-bordered w-full"
           />
           <input
             type="text"
             placeholder="Type here"
-            class="input input-bordered w-full max-w-xs"
+            class="input input-bordered w-full"
+          />
+          <input
+            type="text"
+            placeholder="Type here"
+            class="input input-bordered w-full"
+          />
+          <input
+            type="text"
+            placeholder="Type here"
+            class="input input-bordered w-full"
+          />
+          <input
+            type="text"
+            placeholder="Type here"
+            class="input input-bordered w-full"
+          />
+          <input
+            type="text"
+            placeholder="Type here"
+            class="input input-bordered w-full"
+          />
+          <input
+            type="text"
+            placeholder="Type here"
+            class="input input-bordered w-full"
           />
         </div>
+        <button className="btn btn-primary mt-3">Get Started</button>
       </div>
-    </>
+    </div>
   );
 };
 
