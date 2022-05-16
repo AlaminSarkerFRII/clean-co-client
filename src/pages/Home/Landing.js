44dfedb8400575b7d22bc40f8f665b0b5deb8fde
AlaminSarkerFRII <alamin.education1997@gmail.com>
2022-05-16 20:05:58 +0600

    add animation aos use more

--- src/pages/Home/Landing.js
+++ src/pages/Home/Landing.js
@@ -15,17 +15,39 @@ const Landing = () => {
     <div className="relative">
       <div className="hero h-screen lg:h-[60vh] z-10 relative  bg-accent mt-16">
         <div className="hero-content flex-col lg:flex-row">
-          <div data-aos="fade-right" data-aos-easing="ease-in-sine">
-            <p>Best Quality</p>
-            <h1 className="text-5xl font-bold">
+          <div
+            data-aos="fade-right"
+            ata-aos-duration="100"
+            data-aos-easing="ease-in-sine"
+          >
+            <p data-aos="fade-right" ata-aos-duration="2000">
+              Best Quality
+            </p>
+            <h1
+              data-aos="fade-right"
+              className="text-5xl font-bold"
+              ata-aos-duration="200"
+            >
               Professional Cleaning Service
             </h1>
-            <p className="py-6 max-w-xl">
+            <p
+              data-aos="fade-right"
+              ata-aos-duration="300"
+              data-aos-easing="ease-in-sine"
+              className="py-4 max-w-xl"
+            >
               Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
               excepturi exercitationem quasi. In deleniti eaque aut repudiandae
               et a id nisi.
             </p>
-            <button className="btn btn-primary">Get Started</button>
+            <button
+              data-aos="fade-right"
+              ata-aos-duration="2000"
+              data-aos-easing="ease-in-sine"
+              className="btn btn-primary"
+            >
+              Get Started
+            </button>
           </div>
           <div className="h-[60vh] shrink-0">
             <img src={BucketGirl} className="h-full" alt="" />
@@ -35,7 +57,12 @@ const Landing = () => {
 
       {/* input field */}
 
-      <div className="p-10 rounded-2xl relative shadow-lg mt-[-30px] w-5/6 mx-auto z-20 bg-base-200">
+      <div
+        className="p-10 rounded-2xl relative shadow-lg mt-[-30px] w-5/6 mx-auto z-20 bg-base-200"
+        data-aos="fade-down"
+        data-aos-duration="1000"
+        data-aos-easing="ease-in-sine"
+      >
         <h2 className="text-2xl mb-5">Get Features</h2>
         <div className="grid grid-cols-1 lg:grid-cols-4 md:grid-cols-2 gap-4 mt-5 ">
           <input
