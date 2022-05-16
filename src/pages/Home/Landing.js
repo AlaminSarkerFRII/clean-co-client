168a33889b58e7414791e40396bc2118a9782a58
AlaminSarkerFRII <alamin.education1997@gmail.com>
2022-05-16 19:14:23 +0600

    add features

--- src/pages/Home/Landing.js
+++ src/pages/Home/Landing.js
@@ -3,21 +3,50 @@ import BucketGirl from "../../assets/image/bucketgirl.png";
 
 const Landing = () => {
   return (
-    <div className="hero h-[60vh] bg-base-200 bg-accent mt-16">
-      <div className="hero-content flex-col lg:flex-row">
-        <div>
-          <p>Best Quality</p>
-          <h1 className="text-5xl font-bold">Professional Cleaning Service</h1>
-          <p className="py-6 max-w-xl">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
-          </p>
-          <button className="btn btn-primary">Get Started</button>
+    <>
+      <div className="hero h-screen lg:h-[60vh]  bg-accent mt-16">
+        <div className="hero-content flex-col lg:flex-row">
+          <div>
+            <p>Best Quality</p>
+            <h1 className="text-5xl font-bold">
+              Professional Cleaning Service
+            </h1>
+            <p className="py-6 max-w-xl">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
+              et a id nisi.
+            </p>
+            <button className="btn btn-primary">Get Started</button>
+          </div>
+          <div className="h-[60vh] shrink-0">
+            <img src={BucketGirl} className="h-full" alt="" />
+          </div>
         </div>
-        <img src={BucketGirl} className="max-w-sm rounded-lg h-full" alt="" />
       </div>
-    </div>
+
+      {/* input field */}
+
+      <div className="p-10 rounded-2xl shadow-lg mt-[-30px] w-5/6 mx-auto z-50 bg-neutral">
+        <h2 className="text-primary text-2xl p-2.5">Get Features</h2>
+        <div>
+          <input
+            type="text"
+            placeholder="Type here"
+            class="input input-bordered w-full max-w-xs"
+          />
+          <input
+            type="text"
+            placeholder="Type here"
+            class="input input-bordered w-full max-w-xs"
+          />
+          <input
+            type="text"
+            placeholder="Type here"
+            class="input input-bordered w-full max-w-xs"
+          />
+        </div>
+      </div>
+    </>
   );
 };
 
